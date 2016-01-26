806cf50b412770d04f07dd5efb38cdf1736a8a10
Kyle Cochran <afiftyfootjello@gmail.com>
2016-01-26 13:16:13 -0500

    This is a test commit

---
+++ src/test/TestClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author kyle
+ *
+ */
+public final class TestClass {
+
+	/**
+	 * 
+	 */
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

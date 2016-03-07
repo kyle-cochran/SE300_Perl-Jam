45df4c11c970a5d24b0452c594d950afa4ea2b45
Taylor <hestertaylor2014@gmail.com>
2016-03-06 20:15:43 -0500

    added comments

--- Riddle_Run_Around_Parking/src/DisplayUI.java
+++ Riddle_Run_Around_Parking/src/DisplayUI.java
@@ -26,18 +26,31 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.*;
  
 public class DisplayUI extends Pane {
+	//create new borderpane 
 	BorderPane borderpane = new BorderPane ();
+	/*call methods to create a rectangle, button and vbox
+	 *their return value is then set to a corresponding variable 
+	 * so that the create objects can be displayed in a stage
+	 */
+	
 	Rectangle r = addRectangle();
 	Button PHbutton= buttonHistory();
 	VBox vbox= addVbox();
 	
+	
+	/* creates a new method that creates a new button that when clicked
+	 * will display the parking history
+	 */
 	public Button buttonHistory(){
 		Button buttonPHistory=new Button("Parking History");
 		buttonPHistory.setPrefSize(100,20);
 		
 		return buttonPHistory;
 	}
-
+	
+	/* creates a new method that creates a vbox that will
+	 * display the data regarding the parking availability 
+	 */
 	public VBox addVbox() {
 		VBox vbox=new VBox();
 		//vbox.setPadding(new Insets(10));
@@ -51,6 +64,9 @@ public class DisplayUI extends Pane {
 		return vbox;
 		
 	}
+	/* creates a new method that creates a rectangle that will
+	 * display the image of the parking lot
+	 */
 
 	public Rectangle addRectangle() {
 		Rectangle rectangle = new Rectangle();
@@ -69,18 +85,23 @@ public class DisplayUI extends Pane {
 		
 	
 	public void start(Stage primaryStage) throws Exception{
+		//creates a new scene 
 		Scene scene= new Scene(borderpane);
+		//sets the created button and vbox to a location within the border pane
 		borderpane.setTop(PHbutton); 
 		borderpane.setLeft(vbox);
 		
+		//creates a new pane that will display the parking lot with highlighted spots
 		Pane pane = new Pane();
 		pane.setBackground(new Background(new BackgroundImage(new Image("image directory string"), 
 				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, 
 				new BackgroundSize(100,100,true,true,true,true))));
 		pane.getChildren().add(r);
 		
+		//sets pane to the center of border pane
 		borderpane.setCenter(pane);
 		
+		//displays the scene with the title
 		primaryStage.setScene(scene);
 		primaryStage.setMaximized(true);
 		primaryStage.setTitle("Riddle Run Around Parking");

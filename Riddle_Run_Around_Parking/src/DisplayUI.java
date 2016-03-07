c3375ba409d194eac88ef28a7e688836f69a6452
Taylor <hestertaylor2014@gmail.com>
2016-03-06 19:33:42 -0500

    Changes to the Display UI have been made so the rectangle will display the parking image and have given the start method to main.

--- Riddle_Run_Around_Parking/src/DisplayUI.java
+++ Riddle_Run_Around_Parking/src/DisplayUI.java
@@ -6,12 +6,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -21,23 +27,17 @@ import javafx.scene.shape.*;
  
 public class DisplayUI extends Pane {
 	BorderPane borderpane = new BorderPane ();
-	Rectangle r = addRetangle();
-	
-	HBox hbox= addHBox();
+	Rectangle r = addRectangle();
+	Button PHbutton= buttonHistory();
 	VBox vbox= addVbox();
 	
-	public HBox addHBox() {
-		HBox hbox = new HBox();
-		//hbox.setPadding(new Insets(15, 12, 15, 12));
-		//hbox.setSpacing(10);
-		hbox.setStyle("-fx-background-color: #336699;");
-		
+	public Button buttonHistory(){
 		Button buttonPHistory=new Button("Parking History");
 		buttonPHistory.setPrefSize(100,20);
+		
+		return buttonPHistory;
+	}
 
-		return hbox;
-	}	
-	
 	public VBox addVbox() {
 		VBox vbox=new VBox();
 		//vbox.setPadding(new Insets(10));
@@ -52,7 +52,7 @@ public class DisplayUI extends Pane {
 		
 	}
 
-	public Rectangle addRetangle() {
+	public Rectangle addRectangle() {
 		Rectangle rectangle = new Rectangle();
 		r.setX(50);
 		r.setY(50);
@@ -60,7 +60,7 @@ public class DisplayUI extends Pane {
 		r.setHeight(100);
 		r.setArcWidth(20);
 		r.setArcHeight(20);
-		
+		r.setFill(null);
 		// set background to parking lot
 		
 		return rectangle;
@@ -70,9 +70,17 @@ public class DisplayUI extends Pane {
 	
 	public void start(Stage primaryStage) throws Exception{
 		Scene scene= new Scene(borderpane);
-		borderpane.setTop(hbox); 
+		borderpane.setTop(PHbutton); 
 		borderpane.setLeft(vbox);
-		borderpane.setCenter(r);
+		
+		Pane pane = new Pane();
+		pane.setBackground(new Background(new BackgroundImage(new Image("image directory string"), 
+				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, 
+				new BackgroundSize(100,100,true,true,true,true))));
+		pane.getChildren().add(r);
+		
+		borderpane.setCenter(pane);
+		
 		primaryStage.setScene(scene);
 		primaryStage.setMaximized(true);
 		primaryStage.setTitle("Riddle Run Around Parking");
--- Riddle_Run_Around_Parking/src/RiddleRunAroundParking.java
+++ Riddle_Run_Around_Parking/src/RiddleRunAroundParking.java
@@ -1,12 +1,13 @@
-
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * @author Kyle
  * @version 1.0
  * @created 18-Feb-2016 11:36:22 AM
  */
-public class RiddleRunAroundParking {
-
+public class RiddleRunAroundParking extends Application{
+	
 	private static ImageProcessor imageProc = new ImageProcessor();
 	
 	public RiddleRunAroundParking(){
@@ -22,6 +23,13 @@ public class RiddleRunAroundParking {
 	 */
 	public static void main(String[] args){
 		imageProc.Process();
+		launch(args);
 		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		DisplayUI ui = new DisplayUI();
+		ui.start(primaryStage);
 	}
 }//end RiddleRunAroundParking

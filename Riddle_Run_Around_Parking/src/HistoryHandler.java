1d2963843da7467538efa80164b232dc19ff1432
Kyle Cochran <afiftyfootjello@gmail.com>
2016-03-28 00:54:51 -0400

    I added functionality to the history handler that writes the lot states to a file with the corresponding dates and times. I am using the native JDK DOM to write them as XML.

--- Riddle_Run_Around_Parking/src/HistoryHandler.java
+++ Riddle_Run_Around_Parking/src/HistoryHandler.java
@@ -1,5 +1,12 @@
 import java.io.*;
-import java.util.GregorianCalendar;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.*;
+
+
+
 
 
 /**
@@ -10,60 +17,66 @@ import java.util.GregorianCalendar;
 public class HistoryHandler {
 
 	private File historyFile;
-	private GregorianCalendar dateKeeper;
-	private BufferedWriter writer;
-	private BufferedReader reader;
-	
+	DocumentBuilderFactory dbFactory;
+	DocumentBuilder dBuilder;
 	
 	
 	public HistoryHandler(){
 		historyFile = new File("src/7_day_history.txt");
-		dateKeeper = new GregorianCalendar();
+
 	}
 
-	@SuppressWarnings("deprecation")
+
 	public void commitData(boolean[] spotStates){
-		
-		String currLine;
-		
-		dateKeeper.getTime().getDate();
-		dateKeeper.getTime().getHours();
-		dateKeeper.getTime().getMinutes();
-		
-		try{
-        
-		writer = new BufferedWriter(new FileWriter(historyFile)); 
-        reader = new BufferedReader(new FileReader(historyFile));
-        
-        reader.mark(300);
-        
-        while(!(currLine=reader.readLine()).equals(null)){
-        
-        	if(currLine.charAt(0)!='#' && currLine.charAt(0)!=' '){
-        		
-        	}
-        
-        }
-        
-        
-		} catch (Exception e) {
-	            e.printStackTrace();
-	            
-	        } finally {
-	            try {
-	                // Close the writer regardless of what happens...
-	                writer.close();
-	                reader.close();
-	            } catch (Exception e) {
-	            }
-	        }
-        
-        
-        
 	}
 
-	public boolean[] retrieveData(boolean[] array){
-		return null;
+	
+	
+	public void retrieveData(boolean[] array){
+		try {	
+	         dbFactory = DocumentBuilderFactory.newInstance();
+	         dBuilder = dbFactory.newDocumentBuilder();
+	         
+	         Document doc = dBuilder.parse(historyFile);
+	         doc.getDocumentElement().normalize();
+	         
+	         System.out.println("Root element :" 
+	            + doc.getDocumentElement().getNodeName());
+	         NodeList nList = doc.getElementsByTagName("student");
+	         System.out.println("----------------------------");
+	         for (int temp = 0; temp < nList.getLength(); temp++) {
+	            Node nNode = nList.item(temp);
+	            System.out.println("\nCurrent Element :" 
+	               + nNode.getNodeName());
+	            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	               Element eElement = (Element) nNode;
+	               System.out.println("Student roll no : " 
+	                  + eElement.getAttribute("rollno"));
+	               System.out.println("First Name : " 
+	                  + eElement
+	                  .getElementsByTagName("firstname")
+	                  .item(0)
+	                  .getTextContent());
+	               System.out.println("Last Name : " 
+	               + eElement
+	                  .getElementsByTagName("lastname")
+	                  .item(0)
+	                  .getTextContent());
+	               System.out.println("Nick Name : " 
+	               + eElement
+	                  .getElementsByTagName("nickname")
+	                  .item(0)
+	                  .getTextContent());
+	               System.out.println("Marks : " 
+	               + eElement
+	                  .getElementsByTagName("marks")
+	                  .item(0)
+	                  .getTextContent());
+	            }
+	         }
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
 	}
 
 	public boolean[][] retrieveData(boolean[][] array){
